f8f8cfa0c9a16a81e9b565a1838b2c2179e34f94
LuisHRodrigues <luishenriquebr505@gmail.com>
2026-05-13 09:18:06 -0300

    feat: Implementar responsividade para dispositivos moveis

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -3,10 +3,13 @@ import { LogoComponent } from './logo/logo.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OptionsUserComponent } from './options-user/options-user.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLinkActive,
     RouterLink,
     LogoComponent,
@@ -19,11 +22,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class HeaderComponent {
   menuOpen = false;
 
-  toggleMenu(): void {
+  toggleMenu() {
     this.menuOpen = !this.menuOpen;
-  }
-
-  closeMenu(): void {
-    this.menuOpen = false;
   }
 }
